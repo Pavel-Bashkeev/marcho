12809813b1633f25a1dd87c1d5cca7f9030c743a
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2020-12-03 23:34:36 +0300

    Добалены блоки products, timer, partners, blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,80 @@
-$(function(){
-      $('.top-slider__inner').slick({
-          dots: true,
-          arrows: false,
-          fade: true,
-          autoplay: true,
-          autoplaySpeed: 2000
-      });
+window.addEventListener('DOMContentLoaded', function () {
+  let deadLine = new Date(document.querySelector('.timer-numbers').getAttribute('data-endData'));
 
-      $('.videoBlog__play-btn').fancybox({
-        
-      });
- });
+
+  function getTimeRemaninig(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date()),
+      seconds = Math.floor( (t % (1000 * 60)) / 1000 ),
+      minuts = Math.floor( ( t % (1000 * 60 * 60)) / (1000 * 60) ),
+      hours = Math.floor( (t % ( 1000 * 60 * 60 * 24 ) ) / (1000 * 60 * 60) ),
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minuts': minuts,
+      'seconds': seconds,
+      'days': days
+    };
+
+  }
+  function setClock(id, endTime) {
+    let timer = document.getElementById(id),
+      days = document.querySelector('.days'),
+      hours = timer.querySelector('.hours'),
+      minuts = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(upDateClock, 1000);
+
+
+    function upDateClock() {
+      let t = getTimeRemaninig(endTime);
+
+      if (t.days < 10) {
+        days.textContent = '0' + t.days;
+      } else {
+        days.textContent = t.days;
+      }
+
+      if (t.hours < 10) {
+        hours.textContent = '0' + t.hours;
+      } else {
+        hours.textContent = t.hours;
+      }
+      if (t.minuts < 10) {
+        minuts.textContent = '0' + t.minuts;
+      } else {
+        minuts.textContent = t.minuts;
+      }
+
+      if (t.seconds < 10) {
+        seconds.textContent = '0' + t.seconds;
+      } else {
+        seconds.textContent = t.seconds;
+      }
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+setClock('timer', deadLine);
+});
+
+
+$(function () {
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  $('.videoBlog__play-btn').fancybox({
+
+  });
+  $('.products-item__stars').rateYo({
+    starWidth: "20px",
+    readOnly: true
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ function scripts() {
               'node_modules/jquery/dist/jquery.min.js',
               'node_modules/slick-carousel/slick/slick.min.js',
               'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+              'node_modules/rateyo/src/jquery.rateyo.js',
               'app/js/main.js',
        ])
               .pipe(concat('main.min.js'))
