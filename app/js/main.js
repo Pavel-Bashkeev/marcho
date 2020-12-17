4fdea907589a5c9f87f5de60eeff06a86500a227
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2020-12-17 13:32:27 +0300

    Сделана страница одного блога

--- app/js/main.js
+++ app/js/main.js
@@ -116,7 +116,11 @@ $(function () {
 	// stars
 	$('.products-item__stars').rateYo({
 		starWidth: "20px",
-		readOnly: true
+		readOnly: true,
+		starSvg: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">'+
+			'<path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z">'+
+			'</path>'+
+		'</svg>'
 	});
 
 	// rangeFilter
@@ -173,4 +177,16 @@ $(function () {
 	$('.productItem-form__number').styler();
 	// counter product item
 
+	// слайдер на странице blog
+	$('.blogPage-item__top--slider').slick({
+		infinite: false,
+		prevArrow: '<button type="button" class="slick-prev slick-btn"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">'+
+		'<path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>'+
+		'</svg></button>',
+		nextArrow: '<button type="button" class="slick-next slick-btn"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">'+
+		'<path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>'+
+		'</svg></button>'
+	});
+	// слайдер на странице blog
 });
+
